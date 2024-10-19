1f623701f85cecbd9a3c61ce4a9181072590a0ec
Brayan Acosta <landakkpp@gmail.com>
2024-10-18 19:12:46 -0500

    add search component for test

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import Error from './ui/Error';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
-import Order from './features/order/Order';
+import Order, {loader as orderLoader} from './features/order/Order';
 // import { action as updateOrderAction } from './features/order/UpdateOrder';
 
 import AppLayout from './ui/AppLayout';
+
 
 const router = createBrowserRouter([
   {
@@ -35,8 +36,8 @@ const router = createBrowserRouter([
       {
         path: '/order/:orderId',
         element: <Order />,
-        // loader: orderLoader,
-        // errorElement: <Error />,
+        loader: orderLoader,
+        errorElement: <Error />,
         // action: updateOrderAction,
       },
     ],
