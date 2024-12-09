9a435e54728b10f592a4569a78acec4d6ebb9891
Brayan Acosta <landakkpp@gmail.com>
2024-12-08 22:34:44 -0500

    add store and userSlice

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,7 +1,10 @@
+import { useSelector } from "react-redux";
+
 function Home() {
+  const username = useSelector((state) => state.user.username);
   return (
-    <div>
-      <h1 className="text-xl font-semibold text-center">
+    <div className="my-10 px-4 text-center sm:my-16">
+      <h1 className="mb-8 text-xl font-semibold md:text-3xl">
         The best pizza.
         <br />
         <span className="text-yellow-500">
