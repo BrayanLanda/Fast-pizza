93f19acaccbeeeddca65e9aa185decc2609cb140
Brayan Acosta <landakkpp@gmail.com>
2024-12-08 21:42:00 -0500

    add component button

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -4,21 +4,21 @@ import { Outlet, useNavigation } from "react-router-dom";
 import Loader from "./Loader";
 
 function AppLayout() {
-    const navigation = useNavigation();
-    const isLoading = navigation.state === 'loading';
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
   return (
-    <div className="layout">
-        {isLoading && <Loader />}
+    <div className="grid h-screen grid-rows-[auto_fr_auto]">
+      {isLoading && <Loader />}
       <Header />
-
-      <main>
-        <Outlet />
-      </main>
-
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
       <CartOverview />
     </div>
   );
-  
+
 }
 
 export default AppLayout;
